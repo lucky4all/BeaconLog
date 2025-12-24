c6fc74ffd18524a264572d97e031754c7f3d6322
Lucky4all <lucasromerocoscia@gmail.com>
2025-12-24 09:44:38 -0300

    feat: added zod validation and created the events endpoint

--- backend/src/controllers/event.controller.ts
+++ backend/src/controllers/event.controller.ts
@@ -1,11 +1,32 @@
 import { Hono } from 'hono'
+import { zValidator} from '@hono/zod-validator'
+import { EventRequest } from '../schemas/event.schema.js'
+import { DBService } from '../services/db.service.js'
+import { HTTPException } from 'hono/http-exception'
 
 export const eventController = new Hono()
+const db = new DBService()
 
 eventController.get('/events', async (c) => {
-    try {} catch(error) {}
+    try {
+        
+    } catch(error) {
+
+    }
 })
 
-eventController.post('/events', async (c) => {
-    try {} catch(error) {}
+eventController.post('/events', zValidator('json', EventRequest, (result, c) => {
+    if (!result.success) {
+        return c.json({ message: "Error de validación", error: result.error.issues }, 400);
+    }
+}), async (c) => {
+    try {
+        const validated_data = c.req.valid('json');
+        let token = validated_data.UUID;
+        await db.createEvent(token, validated_data.event)
+        return c.json({ message: "Post creado exitosamente" }, 201);
+    } catch(error) {
+        console.log(error)
+        throw new HTTPException(500, { message: "No se ha podido completar la operación" })
+    }
 })
--- backend/src/schemas/event.schema.ts
+++ backend/src/schemas/event.schema.ts
@@ -1,5 +1,12 @@
+import z from 'zod'
+
 export interface EventSchema {
     PAGE_VIEW: String
     CLICK: String
     SCROLL: String
 }
+
+export const EventRequest = z.object({
+    UUID: z.string().nonempty(),
+    event: z.string().nonoptional(),
+})
