2052870ebf66f469b155c988f4bdaec0867a23e4
Lucky4all <lucasromerocoscia@gmail.com>
2025-12-24 09:45:01 -0300

    fix: removed cookie auth and added auth by Body UUID

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,17 +1,17 @@
 import { Hono } from 'hono'
 import { HTTPException } from 'hono/http-exception'
 import { generateUniqueToken } from '../lib/generateToken.js'
-import { setCookie } from 'hono/cookie'
 
 export const authController = new Hono()
 
 authController.get('/token', (c) => {
     try {
         let token = generateUniqueToken()
-        setCookie(c, "token", token)
+        
         c.status(201)
         return c.json({
-            message: "Token creado exitosamente"
+            message: "Token creado exitosamente",
+            token: token
         })
     } catch(error) {
         console.error(error)
