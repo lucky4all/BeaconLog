cd8310eeb7d43b2ce9613968fd00b46c72988743
Lucky4all <lucasromerocoscia@gmail.com>
2025-12-23 21:05:51 -0300

    fix: removed jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,6 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
 import { secureHeaders } from 'hono/secure-headers'
-import { jwt } from 'hono/jwt'
 import { csrf } from 'hono/csrf'
 import { eventController } from './controllers/event.controller.js'
 
